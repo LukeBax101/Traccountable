575ea6e5125e9b5b469f25a0daa58784c3cfe525
Luke Baxter <lbb29@cam.ac.uk>
2020-04-05 01:08:31 +0100

    URL fix

--- frontend/src/properties.js
+++ frontend/src/properties.js
@@ -1,3 +1,3 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? `${window.location.origin}/ta` : 'http://localhost:5000';
+export const API_URL = process.env.NODE_ENV === 'production' ? `${window.location.origin}/traccountable` : 'http://localhost:5000';
 export const SOCKET_URL = process.env.NODE_ENV === 'production' ? `${window.location.origin}` : 'http://localhost:5000';
-export const SOCKET_PATH = process.env.NODE_ENV === 'production' ? '/ta/socket.io' : '/socket.io';
+export const SOCKET_PATH = process.env.NODE_ENV === 'production' ? '/traccountable/socket.io' : '/socket.io';
